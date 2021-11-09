13245a5a7294574520a9cdece1f3c6b1a5053c5e
Aldrin <aldrinanfouw@gmail.com>
2021-11-09 15:50:10 +0700

    merevisi tampilan laporan persediaan

--- eSoft.LaporanStock/Services/LaporanStockServices.cs
+++ eSoft.LaporanStock/Services/LaporanStockServices.cs
@@ -24,40 +24,70 @@ namespace eSoft.LaporanStock.Services
         private readonly DbContextBeli _contextIR;
         private readonly DbContextJual _contextOE;
 
+        public LaporanStockServices(DbContextPersediaan context, DbContextBeli contextBeli, DbContextJual contextJual)
+        {
+            _context = context;
+            _contextIR = contextBeli;
+            _contextOE = contextJual;
+        }
+
         #region Laporan
         public List<IcStockCardView> CetakMutasi(DateTime Tanggal1, DateTime Tanggal2, string kodeBank)
         {
+             decimal SldAwalJual = 0;
+            decimal SldAwalBeli = 0;
+            decimal SldAwalIC = 0;
+            decimal SaldoAwal = 0;
 
             List<IcStockCardView> Transaksi = new List<IcStockCardView>();
-            //     TransHView Transh = new TransHView() { TransDs = new List<TransDView>() };
+            List<OeTransD> TransAwalJual = new List<OeTransD>();
+            List<IrTransD> TransAwalBeli = new List<IrTransD>();
+            List<IcTransD> TransAwalIC = new List<IcTransD>();
+            List<OeTransH> transHOE = new List<OeTransH>();
+            List<OeTransD> transDOE = new List<OeTransD>();
+            List<IrTransH> transHIR = new List<IrTransH>();
+            List<IrTransD> transDIR = new List<IrTransD>();
+            List<IcTransH> transHIC = new List<IcTransH>();
+            List<IcTransD> transDIC = new List<IcTransD>();
 
-         //   var bankawal = _context.IcItems
-         //       .Where(x => x.ItemCode == kodeBank).FirstOrDefault();
+           
 
-            var TransAwalJual = _contextOE.OeTransDs.Where(x =>x.ItemCode == kodeBank && x.Tanggal < Tanggal1)                
+            TransAwalJual = _contextOE.OeTransDs.Where(x =>x.ItemCode == kodeBank && x.Tanggal < Tanggal1)                
                 .ToList();
-            var TransAwalBeli = _contextIR.IrTransDs.Where(x => x.ItemCode == kodeBank && x.Tanggal < Tanggal1)
+             TransAwalBeli = _contextIR.IrTransDs.Where(x => x.ItemCode == kodeBank && x.Tanggal < Tanggal1)
                 .ToList();
-            var TransAwalIC = _context.IcTransDs.Where(x => x.ItemCode == kodeBank && x.Tanggal < Tanggal1)
+             TransAwalIC = _context.IcTransDs.Where(x => x.ItemCode == kodeBank && x.Tanggal < Tanggal1)
                 .ToList();
 
-            //Transaksi   
-            // .Where(x => x.ItemCode == kodeBank && ( x.Tanggal < Tanggal1))
-            //.Select(x => new RekeningView
-            //{
-            //    KodeBank = x.KodeBank,
-            //    DocNo = x.DocNo,
-            //    Tanggal = x.Tanggal,
-            //    Keterangan = x.Keterangan,
-            //    Kurs = x.Kurs,
-            //    Saldo = (string.IsNullOrEmpty(bankawal.Kurs) ? x.Saldo : x.KSaldo)
+           
+            if (TransAwalJual != null)
+            {
+                SldAwalJual = TransAwalJual.Sum(x => (x.Kode == "94" ? -1 * x.Qty : x.Qty));
+            } else
+            {
+                SldAwalJual = 0;
+            }
 
-            //})
-            // .ToList();
+            if (TransAwalBeli != null)
+            {
+                SldAwalBeli = TransAwalBeli.Sum(x => (x.Kode == "82" ? x.Qty : -1 * x.Qty));
+            }
+            else
+            {
+                SldAwalBeli = 0;
+            }
 
-            var SaldoAwal = TransAwalJual.Sum(x => (x.Kode == "94" ? -1 * x.Qty : x.Qty))
-                            + TransAwalBeli.Sum(x => (x.Kode == "82" ? x.Qty : -1 * x.Qty))
-                            + TransAwalBeli.Sum(x => (x.Kode == "81" ? x.Qty : 0));
+            if (TransAwalIC != null)
+            {
+                SldAwalIC = TransAwalIC.Sum(x => (x.Kode == "81" ? x.QtyShp : 0));
+            }
+            else
+            {
+                SldAwalIC = 0;
+            }
+
+            SaldoAwal = SldAwalBeli + SldAwalIC + SldAwalJual;
+                            
                
 
             Transaksi.Add(new IcStockCardView
@@ -65,32 +95,84 @@ namespace eSoft.LaporanStock.Services
                 ItemCode = kodeBank,               
                 Tanggal = Tanggal1,
                 Keterangan = "Saldo Awal",
-                Jumlah = SaldoAwal
+                Qty = SaldoAwal
 
             }
                 );
 
+            transHIR = _contextIR.IrTransHs.Include(p => p.IrTransDs).Where(x => x.Tanggal >= Tanggal1 && x.Tanggal <= Tanggal2).ToList();
+            transDIR = _contextIR.IrTransDs.Where(x => x.ItemCode == kodeBank).ToList();
 
-            //var Rincian = _context.CbTransHs
-            //    .Where(x => x.KodeBank == kodeBank && (Tanggal1.Date <= x.Tanggal.Date && x.Tanggal.Date <= Tanggal2.Date))
-            //    .OrderBy(x => x.Tanggal)
-            //   .Select(x => new RekeningView
-            //   {
-            //       KodeBank = x.KodeBank,
-            //       DocNo = x.DocNo,
-            //       Tanggal = x.Tanggal,
-            //       Keterangan = x.Keterangan,
-            //       Kurs = x.Kurs,
-            //       Saldo = string.IsNullOrEmpty(bankawal.Kurs) ? x.Saldo : x.KSaldo
+            if (transHIR != null && transDIR != null)
+            {
+                var Rincian1 = (from e in transHIR
+                               join f in transDIR on e.IrTransHId equals f.IrTransHId
+                               select new IcStockCardView()
+                               {
+                                   ItemCode = f.ItemCode,
+                                   Keterangan = e.NamaSup+", "+e.Keterangan,
+                                   Tanggal = f.Tanggal.Date,
+                                   Dokumen = e.NoLpb,
+                                   Qty = (e.Kode == "82" ? f.Qty : -1*f.Qty),
+                                   Cost = f.JumDpp
+                               }).ToList();
+
+                Transaksi.AddRange(Rincian1);
+            }
+
+            transHIC = _context.IcTransHs.Include(p => p.IcTransDs).Where(x => (x.Tanggal >= Tanggal1 && x.Tanggal <= Tanggal2) && x.Kode == "81").ToList();
+            transDIC = _context.IcTransDs.Where(x => x.ItemCode == kodeBank).ToList();
+
+            if (transHIC != null && transDIC != null)
+            {
+                var Rincian2 = (from e in transHIC
+                               join f in transDIC on e.IcTransHId equals f.IcTransHId
+                               select new IcStockCardView()
+                               {
+                                   ItemCode = f.ItemCode,
+                                   Keterangan = e.Keterangan,
+                                   Tanggal = f.Tanggal.Date,
+                                   Dokumen = e.NoFaktur,
+                                   Qty = f.QtyShp,
+                                   Cost = f.Jumlah
+                               }).ToList();
+
+                Transaksi.AddRange(Rincian2);
+            }
 
 
-            //   })
-            //    .ToList();
+            transHOE = _contextOE.OeTransHs.Include(p => p.OeTransDs).Where(x => x.Tanggal >= Tanggal1 && x.Tanggal <= Tanggal2).ToList();
+            transDOE = _contextOE.OeTransDs.Where(x => x.ItemCode == kodeBank).ToList();
 
-            //Transaksi.AddRange(Rincian);
-            //SaldoAwal = 0;
+            if (transHOE != null && transDOE != null)
+            {
+                var Rincian3 = (from e in transHOE
+                               join f in transDOE on e.OeTransHId equals f.OeTransHId
+                               select new IcStockCardView()
+                               {
+                                   ItemCode = f.ItemCode,
+                                   Keterangan = e.NamaCust+", "+e.Keterangan,
+                                   Tanggal = f.Tanggal.Date,
+                                   Dokumen = e.NoLpb,
+                                   Qty = (e.Kode == "94" ? -1*f.Qty :  f.Qty),
+                                   Cost = f.JumDpp
+                               }).ToList();
 
-            //Transaksi = Transaksi.Select(i => { SaldoAwal += i.Saldo; i.Balance = SaldoAwal; return i; }).ToList();
+                Transaksi.AddRange(Rincian3);
+            }
+
+            Transaksi = Transaksi.OrderBy(x =>x.Tanggal).ToList();
+           
+            SaldoAwal = 0;
+
+            Transaksi = Transaksi.Select(i => { SaldoAwal += i.Qty; i.Jumlah = SaldoAwal; return i; }).ToList();
+
+            //foreach (var trans in Transaksi)
+            //{
+            //    SaldoAwal = SaldoAwal + trans.Qty;
+            //    trans.Jumlah = SaldoAwal;
+
+            //}
 
 
             return Transaksi;
