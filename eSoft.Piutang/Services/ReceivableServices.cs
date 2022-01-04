f38c47a41460b0acacc0c92c514a6839420853b2
Aldrin <aldrinanfouw@gmail.com>
2022-01-04 11:22:38 +0700

    laporan uang muka piutang

--- eSoft.Piutang/Services/ReceivableServices.cs
+++ eSoft.Piutang/Services/ReceivableServices.cs
@@ -657,19 +657,34 @@ namespace eSoft.Piutang.Services
             var customer = GetCustomer();
 
 
-            if(trans != null && customer != null)
+            if (trans != null && customer != null)
             {
-                transView = (from header in trans
-                         join detail in customer on header.Customer equals detail.Customer
-                         select new ArPiutngView()
-                         {
-                             Sisa = header.Sisa,
-                             Dokumen = header.Dokumen,
-                             Tanggal = header.Tanggal,
-                             Customer = header.Customer,
-                             NamaCust = detail.NamaCust,     
-                             KdBank = GetTransDoc(header.Dokumen).KdBank
-                         }).ToList();
+                foreach(var header in trans)
+                {
+                    transView.Add(new ArPiutngView()
+                    {
+                        Sisa = header.Sisa,
+                        Dokumen = header.Dokumen,
+                        Tanggal = header.Tanggal,
+                        DueDate = header.DueDate,
+                        Customer = header.Customer,
+                        Keterangan = header.Keterangan,
+                        NamaCust = customer.Find(x =>x.Customer == header.Customer).NamaCust
+                    });
+                }
+                //transView = (from header in trans
+                //             join detail in customer on header.Customer equals detail.Customer
+                //             select new ArPiutngView()
+                //             {
+                //                 Sisa = header.Sisa,
+                //                 Dokumen = header.Dokumen,
+                //                 Tanggal = header.Tanggal,
+                //                 DueDate = header.DueDate,
+                //                 Customer = header.Customer,
+                //                 NamaCust = detail.NamaCust,
+                //                 ArPiutngId = 0
+                //                 //  KdBank = GetTransDoc(header.Dokumen).KdBank
+                //             }).ToList();
             }
 
             return transView;
